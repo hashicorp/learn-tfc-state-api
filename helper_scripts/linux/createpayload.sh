4a6d0ca50fe7213b41f0183e7229f45cef992b6f
Rachel Sharp <rachel@hashicorp.com>
2021-04-12 11:01:46 -0400

    Create createpayload.sh

---
+++ helper_scripts/linux/createpayload.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+serial=$(cat state.tfstate | jq '.serial')
+md5_compute=$(md5sum state.tfstate | awk '{print $1}')
+md5=\"$md5_compute\"
+lineage=$(cat state.tfstate | jq '.lineage')
+base64_encode=$(base64 state.tfstate)
+state=\"$base64_encode\"
+
+
+echo "{
+   \"data\": {
+   \"type\": \"state-versions\",
+     \"attributes\": {
+       \"serial\": $serial,
+       \"md5\": "$md5",
+       \"lineage\": "$lineage",
+       \"state\": "$state"
+     }
+   }
+ }" > payload.json
