902bde601d025a7cfb3016f41196f3734c93fdba
Rachel Sharp <rachel@hashicorp.com>
2021-04-19 15:30:21 -0400

    Update uploadstate.sh

--- helper_scripts/uploadstate.sh
+++ helper_scripts/uploadstate.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
- HTTP_CODE=$(curl \
+ HTTP_POST=$(curl \
      --header "Authorization: Bearer "$tfc_token"" \
      --header "Content-Type: application/vnd.api+json" \
      --request POST \
      --data @payload.json \
      "https://app.terraform.io/api/v2/workspaces/"$workspace_id"/state-versions")
 
- echo $HTTP_CODE
+ echo $HTTP_POST
