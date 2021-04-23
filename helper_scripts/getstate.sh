232a1c16bdd3d13580aa93a9e65ae0e39c7b35f5
Rachel Sharp <rachel@hashicorp.com>
2021-04-23 15:40:32 -0400

    update env vars

--- helper_scripts/getstate.sh
+++ helper_scripts/getstate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 HTTP_RESPONSE=$(curl \
-     --header "Authorization: Bearer "$tfc_token"" \
+     --header "Authorization: Bearer "$TFC_TOKEN"" \
      --header "Content-Type: application/vnd.api+json" \
-     "https://app.terraform.io/api/v2/workspaces/"$workspace_id"/current-state-version" | jq -r '.data | .attributes | ."hosted-state-download-url"')
+     "https://app.terraform.io/api/v2/workspaces/"$WORKSPACE_ID"/current-state-version" | jq -r '.data | .attributes | ."hosted-state-download-url"')
 
 curl -o state.tfstate $HTTP_RESPONSE
--- helper_scripts/uploadstate.sh
+++ helper_scripts/uploadstate.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
  HTTP_POST=$(curl \
-     --header "Authorization: Bearer "$tfc_token"" \
+     --header "Authorization: Bearer "$TFC_TOKEN"" \
      --header "Content-Type: application/vnd.api+json" \
      --request POST \
      --data @payload.json \
-     "https://app.terraform.io/api/v2/workspaces/"$workspace_id"/state-versions")
+     "https://app.terraform.io/api/v2/workspaces/"$WORKSPACE_ID"/state-versions")
 
  echo $HTTP_POST
