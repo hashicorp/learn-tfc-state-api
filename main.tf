664820248b26d182d2f97b64dadd71feb6678d43
Rachel Sharp <rachel@hashicorp.com>
2021-12-06 12:30:42 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,15 @@
 terraform {
+  required_version = ">= 1.1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 3.24.1"
     }
   }
-  required_version = "~> 0.15"
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "<YOUR-ORGANIZATION-NAME>"
-
+  cloud {
+    organization = "<ORGANIZATION_NAME>"
     workspaces {
-      name = "state-versioning"
+      name = "Example-Workspace"
     }
   }
 }
