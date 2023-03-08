c05ba173d12197a7ad035b8aea2a06a0bca09245
Barrett Clark <barrett.clark@hashicorp.com>
2023-03-08 16:46:27 -0600

    Add input file switch to base64 command

--- helper_scripts/createpayload.sh
+++ helper_scripts/createpayload.sh
@@ -7,7 +7,7 @@ serial=$(cat state.tfstate | jq '.serial')
 md5_compute=$(md5 -q state.tfstate)
 md5=\"$md5_compute\"
 lineage=$(cat state.tfstate | jq '.lineage')
-base64_encode=$(base64 state.tfstate)
+base64_encode=$(base64 -i state.tfstate)
 state=\"$base64_encode\"
 
 
